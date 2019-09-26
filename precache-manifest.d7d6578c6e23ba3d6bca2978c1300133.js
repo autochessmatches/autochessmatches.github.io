23aa12c014d6a821a5a7c43be694111739992756
ycyuc2 <kurenai1988@gmail.com>
2019-09-26 22:23:33 +0800

    first commit

---
+++ precache-manifest.d7d6578c6e23ba3d6bca2978c1300133.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fba4feffdf163e461d193a6777486bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "562bf5d3ef7a702c6836",
+    "url": "/static/css/main.6c6241e0.chunk.css"
+  },
+  {
+    "revision": "d30841922db2ecd8d41f",
+    "url": "/static/js/2.9a68a112.chunk.js"
+  },
+  {
+    "revision": "562bf5d3ef7a702c6836",
+    "url": "/static/js/main.358ffa59.chunk.js"
+  },
+  {
+    "revision": "81f889a782326b240fb4",
+    "url": "/static/js/runtime-main.c4f4c9fd.js"
+  }
+]);
